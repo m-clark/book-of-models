3e362e76a22ebe43a2d20af82fd314df080417c2
micl <stats.data.sci@gmail.com>
2023-08-27 20:36:24 -0400

    function to switch words based on sign

--- functions/utils.r
+++ functions/utils.r
@@ -5,3 +5,7 @@ sd <- function(x, na.rm = TRUE) {
 # mean <- function(x, na.rm = TRUE) {
 #     mean(x, na.rm = na.rm)
 # }
+
+word_sign <- function(value, words) {
+    ifelse(sign(value) == 1, words[1], words[2])
+}
